74923f4a3ec4448e233ec5d87f883ed209421cff
kairat-front-and <aptrapov.kairat@gmail.com>
2021-08-26 21:31:12 +0600

    release v1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  
+  return React.createElement("div",null,React.createElement('h1',null,'Makers Bootcamp' ))
+}
+
+export default App;
